8abb560f4d2b6c7fc5f67cd0350535f38f695bf2
Nico Michaelis <86128620+nicomichaelis@users.noreply.github.com>
2022-04-24 08:42:33 +0200

    refactor: separate cli setup from processing

---
+++ Michaelis.QuickTemplates/EntryPoint.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Michaelis.QuickTemplates;
+
+class EntryPoint
+{
+    static async Task<int> Main(string[] args)
+    {
+        return await Main(args, null);
+    }
+
+    static async Task<int> Main(string[] args, IConsole console)
+    {
+        var inputOption = new Option<DirectoryInfo>(
+                "--input",
+                getDefaultValue: () => new DirectoryInfo(Environment.CurrentDirectory),
+                "input directory")
+        {
+            IsRequired = true
+        }.ExistingOnly();
+        inputOption.AddAlias("-i");
+
+        var outputOption = new Option<DirectoryInfo>(
+                "--output",
+                getDefaultValue: () => new DirectoryInfo(Environment.CurrentDirectory),
+                "output directory")
+        {
+            IsRequired = true
+        };
+        outputOption.AddAlias("-o");
+
+        var recursiveOption = new Option<bool>(
+                "--recurse",                
+                "recurse input directory directory")
+        {
+            IsRequired = true
+        };
+        recursiveOption.AddAlias("-r");
+
+        var rootCommand = new RootCommand
+        {
+            inputOption,
+            outputOption,
+            recursiveOption,
+        };
+        rootCommand.Description = "Quick Template Generator" + Environment.NewLine + "For details visit https://github.com/nicomichaelis/QuickTemplates/";
+
+        rootCommand.SetHandler(async (DirectoryInfo input, DirectoryInfo output, bool recurse, InvocationContext ctx) =>
+            {
+                Program handler = new(ctx.Console);
+                ctx.ExitCode = await handler.Run(input, output, recurse, ctx.GetCancellationToken());
+            }, inputOption, outputOption);
+
+        return await rootCommand.InvokeAsync(args, console);
+    }
+}
--- Michaelis.QuickTemplates/Program.cs
+++ Michaelis.QuickTemplates/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
 using System.CommandLine;
+using System.CommandLine.IO;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -9,64 +9,18 @@ namespace Michaelis.QuickTemplates;
 
 class Program
 {
-    static int returnCode = int.MinValue;
+    IConsole Console { get; }
+    DiagnosticsCollection Diagnostics { get; } = new();
 
-    static async Task<int> Main(string[] args)
+    public Program(IConsole console)
     {
-        var inputOption = new Option<DirectoryInfo>(
-                "--input",
-                getDefaultValue: () => new DirectoryInfo(Environment.CurrentDirectory),
-                "input directory")
-        {
-            IsRequired = true
-        }.ExistingOnly();
-
-        var outputOption = new Option<DirectoryInfo>(
-                "--output",
-                getDefaultValue: () => new DirectoryInfo(Environment.CurrentDirectory),
-                "output directory")
-        {
-            IsRequired = true
-        };
-
-
-        var rootCommand = new RootCommand
-        {
-            inputOption,
-            outputOption,
-        };
-
-        rootCommand.Description = "Mini Template Generator";
-
-        rootCommand.SetHandler(async (DirectoryInfo input, DirectoryInfo output, CancellationToken cancel) =>
-            {
-                CreateDirectory(output);
-                returnCode = await RunTemplateGeneratorOnDirectory(input, output, cancel);
-            }, inputOption, outputOption);
-
-        await rootCommand.InvokeAsync(args);
-        return returnCode;
+        Console = console;
     }
 
-    private static void CreateDirectory(DirectoryInfo output)
+    public async Task<int> Run(DirectoryInfo input, DirectoryInfo output, bool recurse, CancellationToken cancel)
     {
-        if (!output.Exists)
-        {
-            Console.WriteLine($"Creating directory '{output.FullName}'");
-            try
-            {
-                output.Create();
-            }
-            catch (IOException e)
-            {
-                Console.Error.WriteLine($"Output directory could not be created: {e.Message}");
-            }
-        }
-    }
-
-    private static async Task<int> RunTemplateGeneratorOnDirectory(DirectoryInfo input, DirectoryInfo output, CancellationToken cancel)
-    {
-        var inputs = input.EnumerateFiles("*.tt", SearchOption.TopDirectoryOnly).OrderBy(z => z.Name, NaturalSortComparer.Default).ToList();
+        var searchOptions = recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+        var inputs = input.EnumerateFiles("*.tt", searchOptions).OrderBy(z => z.Name, NaturalSortComparer.Default).ToList();
 
         DiagnosticsCollection diagnostics = new();
         Generator gen = new Generator(inputs, diagnostics);
@@ -86,7 +40,7 @@ class Program
             {
                 return -3;
             }
-            
+
             await gen.GenerateOutput(input, output, CreateFileWriter);
         }
         finally
@@ -96,16 +50,36 @@ class Program
         return 0;
     }
 
-    private static TextWriter CreateFileWriter(string filename) {
+    private bool CreateDirectory(DirectoryInfo output)
+    {
+        if (!output.Exists)
+        {
+            Console.Out.WriteLine($"Creating directory '{output.FullName}'");
+            try
+            {
+                output.Create();
+            }
+            catch (IOException e)
+            {
+                Console.Error.WriteLine($"Output directory could not be created: {e.Message}");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static TextWriter CreateFileWriter(string filename)
+    {
         var cs = new ChangeStream(File.Open(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read), false);
         return new StreamWriter(cs);
     }
 
-    private static void WriteDiagnostics(DiagnosticsCollection diagnostics)
+    private void WriteDiagnostics(DiagnosticsCollection diagnostics)
     {
         foreach (var diagnostic in diagnostics.Diagnostics.OrderBy(z => z.Message.Location.SourceName, NaturalSortComparer.Default).ThenBy(z => z.Message.Location.Line).ThenBy(z => z.Message.Location.Col))
         {
             Console.Error.WriteLine($"{diagnostic.Message.Location.SourceName}({diagnostic.Message.Location.Line},{diagnostic.Message.Location.Col}): {diagnostic.Severity}: {diagnostic.Message.Text}");
         }
     }
+
 }
